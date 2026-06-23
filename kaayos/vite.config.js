d0afc1695def1be2eca31f56014c1aadaa02f23c
johndavebriones <johndavealdaybriones009@gmail.com>
2026-06-24 05:01:53 +0800

    other files needed for the development

--- kaayos/vite.config.js
+++ kaayos/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/client.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/client.css',
+                'resources/js/app.js',
+                'resources/js/client/account.jsx',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -16,6 +22,7 @@ export default defineConfig({
             ],
         }),
         vue(),
+        react(),
         tailwindcss(),
     ],
     server: {
